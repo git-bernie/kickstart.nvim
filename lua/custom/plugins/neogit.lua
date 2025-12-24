31c1297b6ce7c30ec2bf34b81aabd9eeab148fb9
git-bernie <bernie.david@gmail.com>
2025-12-24 10:16:49 -0800

    chore: adding clarifying comments to the neogit plugin

--- lua/custom/plugins/neogit.lua
+++ lua/custom/plugins/neogit.lua
@@ -1,5 +1,15 @@
+--[[ 
+-- @see https://github.com/NeogitOrg/neogit
+:Neogit             " Open the status buffer in a new tab
+:Neogit cwd=<cwd>   " Use a different repository path
+:Neogit cwd=%:p:h   " Uses the repository of the current file
+:Neogit kind=<kind> " Open specified popup directly
+:Neogit commit      " Open commit popup
+
+--]]
 return {
   'NeogitOrg/neogit',
+  lazy = true,
   dependencies = {
     'nvim-lua/plenary.nvim', -- required
     'sindrets/diffview.nvim', -- optional - Diff integration
