514a6f96f476ae0354f4c8a899e11b0dd66898e1
git-bernie <bernie.david@gmail.com>
2026-01-28 11:55:38 -0800

    chore(leap): plugin source/url changed to codeberg

--- lua/custom/plugins/leap.lua
+++ lua/custom/plugins/leap.lua
@@ -1,7 +1,10 @@
 -- leap (replacement for sneak?)
 -- https://github.com/ggandor/leap.nvim
 return {
-  'ggandor/leap.nvim',
+  -- 'ggandor/leap.nvim',
+  -- change the url to `https://codeberg.org/andyg/leap.nvim`.
+  -- 'andyg/leap.nvim',
+  url = 'https://codeberg.org/andyg/leap.nvim',
   enabled = true,
   -- Optional dependency
   opts = {
@@ -11,7 +14,7 @@ return {
   keys = {
     { 's', '<Plug>(leap-forward)', noremap = false, desc = '[s]neak (Leap)', mode = { 'n', 'x', 'o' } },
     -- { 'ss', '<Plug>(leap-forward)', noremap = false, desc = '[s]neak (Leap)', mode = { 'n', 'x', 'o' } },
-    { 'gs', '<Plug>(leap-forward)', noremap = false, desc = '[s]neak (Leap)', mode = { 'n', 'x', 'o' } },
+    -- { 'gs', '<Plug>(leap-forward)', noremap = false, desc = '[s]neak (Leap)', mode = { 'n', 'x', 'o' } },
     { 'S', '<Plug>(leap-backward)', noremap = false, desc = '[S]neak (Leap) backwards', mode = { 'n', 'x', 'o' } },
     --[{ 'gs', '<Plug>(leap-from-window)', noremap = false, desc = '[gs]neak from window', mode = { 'n', 'x', 'o' } },]]
   },
